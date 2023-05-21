fba247be09d0390fa33886206887024e7404dc3c
ABDULLAH_Q\abdul <abdullah6930@gmail.com>
2023-05-21 10:47:51 +0500

    # Added basic socket mechanic

---
+++ MultiplayerServer/Assets/Scripts/GameServer.cs
@@ -0,0 +1,78 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace MultiplayerServer.GS;
+
+public class GameServer
+{
+    private Socket? serverSocket;
+    private readonly byte[] buffer = new byte[1024]; // Buffer for receiving data
+
+    public void Start(string ipAddressString, int port)
+    {
+        // Create the server socket
+        serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the server socket to a specific IP address and port
+        IPAddress ipAddress = IPAddress.Parse(ipAddressString);
+        IPEndPoint endPoint = new IPEndPoint(ipAddress, port);
+        serverSocket.Bind(endPoint);
+
+        // Start listening for incoming client connections
+        serverSocket.Listen(5); // Maximum number of queued connections
+
+        Console.WriteLine("Server started. Waiting for client connections...");
+
+        // Start accepting client connections in a separate thread
+        Thread acceptThread = new (AcceptConnections);
+        acceptThread.Start();
+    }
+
+    private void AcceptConnections()
+    {
+        while (true)
+        {
+            // Accept an incoming client connection
+            Socket clientSocket = serverSocket.Accept();
+
+            Console.WriteLine("Client connected: " + clientSocket.RemoteEndPoint);
+
+            // Start a new thread to handle the client connection
+            Thread clientThread = new (HandleClient);
+            clientThread.Start(clientSocket);
+        }
+    }
+
+    private void HandleClient(object clientSocketObj)
+    {
+        Socket clientSocket = (Socket)clientSocketObj;
+
+        // Handle the client connection
+        try
+        {
+            while (true)
+            {
+                // Receive data from the client
+                int bytesRead = clientSocket.Receive(buffer);
+                string message = System.Text.Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received from client: " + message);
+
+                // Process the received data or perform any necessary actions
+
+                // Example: Send a response back to the client
+                string response = "Hello from server!";
+                byte[] responseBytes = System.Text.Encoding.ASCII.GetBytes(response);
+                clientSocket.Send(responseBytes);
+            }
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine("Client disconnected: " + ex.Message);
+        }
+        finally
+        {
+            // Close the client socket
+            clientSocket.Close();
+        }
+    }
+}
--- MultiplayerServer/Program.cs
+++ MultiplayerServer/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MultiplayerServer.GS;
+using System.Text.Json;
+
+namespace MultiplayerServer;
+
+class ServerConfig
+{
+    public string? CurrentEnvironment { get; set; }
+    public EnvironmentConfig? Development { get; set; }
+    public EnvironmentConfig? Production { get; set; }
+}
+
+class EnvironmentConfig
+{
+    public string? IPAddress { get; set; }
+    public int Port { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Read the server configuration from ServerConfig.json
+        string configPath = Path.Combine("Assets", "Config", "ServerConfig.json");
+        string configJson = File.ReadAllText(configPath);
+        var serverConfig = JsonSerializer.Deserialize<ServerConfig>(configJson);
+
+        // Retrieve the appropriate IP address based on the current environment
+        var environmentConfig = GetEnvironmentConfig(serverConfig);
+
+        // Use the IP address for your server setup
+        GameServer server = new GameServer();
+        server.Start(environmentConfig.IPAddress, environmentConfig.Port);
+        // Example usage:
+        Console.WriteLine("Server started. Using IP address: " + environmentConfig.IPAddress);
+
+        // Keep the server running until the user presses Enter
+        Console.ReadLine();
+    }
+
+    static EnvironmentConfig GetEnvironmentConfig(ServerConfig serverConfig)
+    {
+        // Retrieve the appropriate IP address based on the current environment
+        switch (serverConfig.CurrentEnvironment.ToLower())
+        {
+            case "development":
+                return serverConfig.Development;
+            case "production":
+                return serverConfig.Production;
+            default:
+                throw new ArgumentException("Invalid CurrentEnvironment value in the configuration file.");
+        }
+    }
+}
